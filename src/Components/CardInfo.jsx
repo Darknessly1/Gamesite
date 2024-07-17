c120abb6db4f4233d536ab9bbedb7455adeb202b
Amine lehkim. <aminelehkim158@gmail.com>
2024-07-17 01:12:49 +0100

    small changes

--- src/Components/CardInfo.jsx
+++ src/Components/CardInfo.jsx
@@ -6,55 +6,94 @@ const CardModal = ({ card, onClose }) => {
     // if (!card) return null;
 
     const [imageUrl, setImageUrl] = useState('');
+    const [borderImageUrl, setBorderImageUrl] = useState('');
+    const [provisionIconUrl, setProvisionIconUrl] = useState('');
+    const [provisionFactionUrl, setProvisionFactionUrl] = useState('');
+    const [provisionNumberUrl, setProvisionNumberUrl] = useState('');
+    const [rarityIconUrl, setRarityIconUrl] = useState('');
+    const [powerImageUrl, setPowerImageUrl] = useState('');
 
     useEffect(() => {
-        const fetchImageUrl = async () => {
+        const fetchImageUrls = async () => {
             try {
                 const response = await fetch(`https://api.gwent.one/?key=data&id=${card.id.card}&response=html&html=version.artsize.linkart&class=rounded&version=1.1.0`);
                 const text = await response.text();
                 const parser = new DOMParser();
                 const doc = parser.parseFromString(text, 'text/html');
-                const imgElement = doc.querySelector('.G1-art img'); // Adjust the selector based on the actual structure
-                if (imgElement) {
-                    setImageUrl(imgElement.src);
+
+                const cardArtElement = doc.querySelector('.G1-cardart');
+                if (cardArtElement) {
+                    const images = cardArtElement.querySelectorAll('img');
+
+                    images.forEach((img) => {
+                        const src = img.getAttribute('src');
+                        if (src.includes('card/art')) {
+                            setImageUrl(src);
+                        } else if (src.includes('border_gold')) {
+                            setBorderImageUrl(src);
+                        } else if (src.includes('provision_icon')) {
+                            setProvisionIconUrl(src);
+                        } else if (src.includes('provision_neutral')) {
+                            setProvisionFactionUrl(src);
+                        } else if (src.includes('provision_')) {
+                            setProvisionNumberUrl(src);
+                        } else if (src.includes('rarity_legendary')) {
+                            setRarityIconUrl(src);
+                        } else if (src.includes('power_')) { // This should match the power image URL
+                            setPowerImageUrl(src);
+                        }
+                    });
                 }
             } catch (error) {
                 console.error('Error fetching image:', error);
             }
         };
 
-        if (card.id.card) {
-            fetchImageUrl();
+        if (card.id.art) {
+            fetchImageUrls();
         }
-    }, [card.id.card]);
+    }, [card.id.art]);
 
     return (
         <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
-            <div className="m-4 bg-white max-w-4xl shadow sm:rounded-lg h-full max-h-screen">
-                <div className="px-4 py-5 sm:px-6 flex justify-between">
-                    <h3 className="text-lg leading-6 font-medium text-gray-900">
+            <div className="  bg-white max-w-4xl shadow sm:rounded-lg h-full max-h-screen">
+                <div className="px-4 py-5 sm:px-6 flex justify-between bg-nt-header-bg">
+                    <h3 className="text-lg leading-6 font-medium text-white ">
                         Card Details
                     </h3>
-                    <button onClick={onClose} className="text-red-500 font-medium">Close</button>
+                    <button onClick={onClose} className="text-red-400 font-medium ">Close</button>
                 </div>
 
                 <div className="flex border-t border-gray-200 h-full">
-                    <div className="w-1/2 flex items-center justify-center relative">
-                        {imageUrl ? (
-                            <div className="relative">
-                                <img src={imageUrl} alt={card.name} className="object-cover rounded-md max-h-96" />
-                                <img src="https://gwent.one/image/gwent/assets/card/other/medium/border_gold.png" alt="Border" className="absolute inset-0 w-full h-full object-cover" />
-                                <img src="https://gwent.one/image/gwent/assets/card/banner/medium/provision_icon.png" alt="Provision Icon" className=" absolute top-2 right-2 w-full h-full" />
-                                <img src="https://gwent.one/image/gwent/assets/card/banner/medium/provision_neutral.png" alt="Provision Banner" className="absolute top-2 left-2 w-full h-full" />
-                                <img src="https://gwent.one/image/gwent/assets/card/number/medium/provision_8.png" alt="Provision Number" className="absolute top-2 left-12 w-full h-full" />
-                                <img src="https://gwent.one/image/gwent/assets/card/banner/medium/default_neutral.png" alt="Default Banner" className="absolute bottom-2 left-2 w-full h-full" />
-                                <img src="https://gwent.one/image/gwent/assets/card/number/medium/power_4.png" alt="Power Number" className="absolute bottom-2 left-12 w-full h-full" />
-                                <img src="https://gwent.one/image/gwent/assets/card/other/medium/rarity_legendary.png" alt="Rarity Icon" className="absolute top-2 left-20 w-full h-full" />
-                            </div>
-                        ) : (
-                            <p>No image available</p>
-                        )}
+
+                    <div className="md:flex-1 flex flex-col items-center justify-center">
+                        <div className="relative">
+                            {imageUrl && (
+                                <img className="h-full w-full  mb-16" src={imageUrl} alt={card.name} />
+                            )}
+                            {borderImageUrl && (
+                                <img className="absolute inset-0 w-full h-full mb-16 " src={borderImageUrl} alt="Border" />
+                            )}
+                            {provisionIconUrl && (
+                                <img className="absolute top-0 left-0 w-full h-full mb-16" src={provisionIconUrl} alt="Provision Icon" />
+                            )}
+                            {provisionFactionUrl && (
+                                <img className="absolute top-0 left-0 w-full h-full mb-16" src={provisionFactionUrl} alt="Provision Faction" />
+                            )}
+                            <img src="https://gwent.one/image/gwent/assets/card/banner/medium/default_neutral.png" alt="Default Banner" className="absolute bottom-1 w-full h-full " />
+                            {provisionNumberUrl && (
+                                <img className="absolute top-0 left-0 w-full h-full mb-16" src={provisionNumberUrl} alt="Provision Number" />
+                            )}
+                            {rarityIconUrl && (
+                                <img className="absolute bottom-1 left-0 w-full h-full " src={rarityIconUrl} alt="Rarity Icon" />
+                            )}
+
+                            {powerImageUrl && (
+                                <img className="absolute bottom-1 left-0 w-full h-full " src={powerImageUrl} alt="Power" />
+                            )}
+                        </div>
                     </div>
+
 
                     <div className="w-1/2 overflow-y-auto">
                         <dl>
@@ -203,3 +242,21 @@ const CardModal = ({ card, onClose }) => {
 };
 
 export default CardModal;
+
+
+{/* <div className="w-1/2 flex items-center justify-center relative">
+    {imageUrl ? (
+        <div className="relative">
+            <img src={imageUrl} alt={card.name} className="object-cover rounded-md max-h-96" />
+            <img src="https://gwent.one/image/gwent/assets/card/other/medium/border_gold.png" alt="Border" className="absolute inset-0 w-full h-full object-cover" />
+            <img src="https://gwent.one/image/gwent/assets/card/banner/medium/provision_icon.png" alt="Provision Icon" className=" absolute top-2 right-2 w-full h-full" />
+            <img src="https://gwent.one/image/gwent/assets/card/banner/medium/provision_neutral.png" alt="Provision Banner" className="absolute top-2 left-2 w-full h-full" />
+            <img src="https://gwent.one/image/gwent/assets/card/number/medium/provision_8.png" alt="Provision Number" className="absolute top-2 left-12 w-full h-full" />
+            <img src="https://gwent.one/image/gwent/assets/card/banner/medium/default_neutral.png" alt="Default Banner" className="absolute bottom-2 left-2 w-full h-full" />
+            <img src="https://gwent.one/image/gwent/assets/card/number/medium/power_4.png" alt="Power Number" className="absolute bottom-2 left-12 w-full h-full" />
+            <img src="https://gwent.one/image/gwent/assets/card/other/medium/rarity_legendary.png" alt="Rarity Icon" className="absolute top-2 left-20 w-full h-full" />
+        </div>
+    ) : (
+        <p>No image available</p>
+    )}
+</div> */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ import withMT from "@material-tailwind/html/utils/withMT";
 export default withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'nt-header-bg': '#302113',
+      },
+    },
   },
   plugins: [],
 });
