484a3fbf6d4f8839e8e80b8de32ccda437063b52
Amine lehkim. <aminelehkim158@gmail.com>
2024-08-30 22:46:45 +0100

    Still working on the style

--- src/Pages/ShowGames.jsx
+++ src/Pages/ShowGames.jsx
@@ -10,12 +10,9 @@ const ShowGames = () => {
     const [filteredGames, setFilteredGames] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [error, setError] = useState(null);
-    const [itemType, setItemType] = useState([])
+    const [selectedType, setSelectedType] = useState('all');
     const gamesPerPage = 6;
-    const excludedAppIds = [
-        216938, 660010, 660130, 1122575, 1122577,
-        1122576, 1122579, 1122578
-    ];
+    const excludedAppIds = [216938, 660010, 660130, 1122575, 1122577, 1122576, 1122579, 1122578, 254650, 254671, 254674, 254670, 254673, 254672];
 
     useEffect(() => {
         const fetchGames = async () => {
@@ -26,7 +23,7 @@ const ShowGames = () => {
                         game => game.name && !excludedAppIds.includes(game.appid)
                     );
                     setGames(allGames);
-                    setFilteredGames(allGames); // Initially, show all games
+                    setFilteredGames(allGames);
                 } else {
                     setError('Invalid data structure received from API');
                 }
@@ -37,25 +34,6 @@ const ShowGames = () => {
         };
         fetchGames();
     }, []);
-
-
-
-    useEffect(() => {
-        const filterGamesByType = () => {
-            if (itemType === 'all') {
-                setItemType(games);
-            } else {
-                const filtered = games.filter(game => {
-                    const gameDetails = detailedGames[game.appid];
-                    return gameDetails && gameDetails.type === itemType;
-                });
-                setItemType(filtered);
-            }
-        };
-
-        filterGamesByType();
-    }, [itemType, games, detailedGames]);
-
 
 
     useEffect(() => {
@@ -119,8 +97,23 @@ const ShowGames = () => {
     };
 
     const indexOfLastGame = currentPage * gamesPerPage;
-    const indexOfFirstGame = indexOfLastGame - gamesPerPage;
-    const currentGames = filteredGames.slice(indexOfFirstGame, indexOfLastGame);
+    const indexOfFirstGame = indexOfLastGame - gamesPerPage;npm 
+    // const currentGames = filteredGames.slice(indexOfFirstGame, indexOfLastGame);
+
+    const filterGamesByType = () => {
+        if (selectedType === 'all') {
+            return filteredGames;
+        } else {
+            return filteredGames.filter(game =>
+                detailedGames[game.appid] && detailedGames[game.appid].type === selectedType
+            );
+        }
+    };
+
+    const filteredByTypeGames = filterGamesByType();
+    const currentGames = filteredByTypeGames.slice(indexOfFirstGame, indexOfLastGame);
+
+
 
     return (
         <div className="container mx-auto p-4">
@@ -134,63 +127,63 @@ const ShowGames = () => {
                     className="p-2 border rounded w-fit mr-2 content-center justify-center"
                 />
             </div>
-            <div className=''>
+
+            <div className='mb-4'>
                 <button
-                    className={`bg-black text-white m-2 p-2 ${itemType === 'game' ? 'bg-gray-700' : ''}`}
-                    onClick={() => setItemType('game')}
+                    className={`bg-black text-white m-2 p-2 ${selectedType === 'game' ? 'bg-gray-700' : ''}`}
+                    onClick={() => setSelectedType('game')}
                 >
                     Games
                 </button>
                 <button
-                    className={`bg-black text-white m-2 p-2 ${itemType === 'dlc' ? 'bg-gray-700' : ''}`}
-                    onClick={() => setItemType('dlc')}
+                    className={`bg-black text-white m-2 p-2 ${selectedType === 'dlc' ? 'bg-gray-700' : ''}`}
+                    onClick={() => setSelectedType('dlc')}
                 >
                     DLC
                 </button>
                 <button
-                    className={`bg-black text-white m-2 p-2 ${itemType === 'demo' ? 'bg-gray-700' : ''}`}
-                    onClick={() => setItemType('demo')}
+                    className={`bg-black text-white m-2 p-2 ${selectedType === 'demo' ? 'bg-gray-700' : ''}`}
+                    onClick={() => setSelectedType('demo')}
                 >
                     Demo
                 </button>
                 <button
-                    className={`bg-black text-white m-2 p-2 ${itemType === 'mod' ? 'bg-gray-700' : ''}`}
-                    onClick={() => setItemType('mod')}
+                    className={`bg-black text-white m-2 p-2 ${selectedType === 'mod' ? 'bg-gray-700' : ''}`}
+                    onClick={() => setSelectedType('mod')}
                 >
                     Mods
                 </button>
                 <button
-                    className={`bg-black text-white m-2 p-2 ${itemType === 'all' ? 'bg-gray-700' : ''}`}
-                    onClick={() => setItemType('soundtrack')}
-                >
-                    Soundtrack
-                </button>
-                <button
-                    className={`bg-black text-white m-2 p-2 ${itemType === 'all' ? 'bg-gray-700' : ''}`}
-                    onClick={() => setItemType('all')}
+                    className={`bg-black text-white m-2 p-2 ${selectedType === 'all' ? 'bg-gray-700' : ''}`}
+                    onClick={() => setSelectedType('all')}
                 >
                     All
                 </button>
             </div>
 
+
             {error && <p className="text-red-500">{error}</p>}
+
             <div className="grid grid-cols-3 gap-4">
-                {currentGames.map(game => (
-                    <Link to={`/games/${game.appid}`} key={game.appid} className="bg-white p-4 rounded shadow-md">
-                        <div className="flex flex-col items-center">
-                            {detailedGames[game.appid] && (
-                                <>
-                                    <img
-                                        src={detailedGames[game.appid].header_image}
-                                        className="mb-2"
-                                    />
-                                    <p className="text-center">{game.name}</p>
-                                </>
-                            )}
-                        </div>
-                    </Link>
-                ))}
+                {currentGames
+                    .filter(game => detailedGames[game.appid] && detailedGames[game.appid].header_image)
+                    .map(game => (
+                        <Link to={`/games/${game.appid}`} key={game.appid} className="bg-white p-4 rounded shadow-md">
+                            <div className="flex flex-col items-center">
+                                <img
+                                    src={detailedGames[game.appid].header_image}
+                                    className="mb-2"
+                                    alt={game.name}
+                                />
+                                <p className="text-center font-bold">{game.name}</p>
+                                <p className="text-center text-sm text-gray-600">
+                                    {detailedGames[game.appid].type || 'Unknown Type'}
+                                </p>
+                            </div>
+                        </Link>
+                    ))}
             </div>
+
             <Pagination
                 cardsPerPage={gamesPerPage}
                 totalCards={filteredGames.length}
