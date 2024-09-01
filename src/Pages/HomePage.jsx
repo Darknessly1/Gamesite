78870f7b1dfcd2ef9de52d74950002c1ca4dfe79
Amine lehkim. <aminelehkim158@gmail.com>
2024-09-01 12:52:10 +0100

    Start working on the home page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,168 @@
-// import ShowGames from './ShowGames'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Pagination from '../Components/Pagination';
 
-export default function HomePage() {
-    return(
-        <>
-            {/* <ShowGames /> */}
-        </>
+const HomePage = () => {
+    const [news, setNews] = useState([]);
+    const [dailyDeals, setDailyDeals] = useState([]);
+    const [newReleases, setNewReleases] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [dealsPage, setDealsPage] = useState(1);
+    const [releaasePage, setReleasePage] = useState(1);
+    const [totalDeals, setTotalDeals] = useState(0);
+    const [error, setError] = useState(null);
+    const newsPerPage = 4;
+    const dealsPerPage = 2;
+    const relPerPage = 2;
+
+    useEffect(() => {
+        const fetchNews = async () => {
+            try {
+                const response = await axios.get('http://localhost:4000/api/news');
+                setNews(response.data);
+            } catch (err) {
+                console.error('Error fetching news:', err.message);
+                setError('Error fetching news');
+            }
+        };
+
+        const fetchDailyDeals = async () => {
+            try {
+                const response = await axios.get(`http://localhost:4000/api/sales?page=${dealsPage}&limit=${dealsPerPage}`);
+                setDailyDeals(response.data.salesData);
+                setTotalDeals(response.data.totalItems);
+            } catch (err) {
+                console.error('Error fetching daily deals:', err.message);
+                setError('Error fetching daily deals');
+            }
+        };
+
+        const fetchNewReleases = async () => {
+            try {
+                const response = await axios.get('http://localhost:4000/api/new-releases?page=${releaasePage}&limit=${relPerPage}');
+                setNewReleases(response.data);
+            } catch (err) {
+                console.error('Error fetching new releases:', err.message);
+                setError('Error fetching new releases');
+            }
+        };
+
+        fetchNews();
+        fetchDailyDeals();
+        fetchNewReleases();
+    }, [dealsPage]);
+
+    const indexOfLastNews = currentPage * newsPerPage;
+    const indexOfFirstNews = indexOfLastNews - newsPerPage;
+    const currentNews = news.slice(indexOfFirstNews, indexOfFirstNews + newsPerPage);
+
+    const handleNewsPageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    const handleDealsPageChange = (pageNumber) => {
+        setDealsPage(pageNumber);
+    };
+
+    const handleRelPageChange = (pageNumber) => {
+        setReleasePage(pageNumber);
+    };
+
+    
+
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-4">Latest News & Daily Deals</h1>
+
+            {error && <p className="text-red-500">{error}</p>}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+                <div className="news-section w-fit">
+                    <h2 className="text-xl font-bold mb-4">Latest News</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        {currentNews.map((item, index) => (
+                            <div key={index} className="bg-white p-4 shadow-md rounded-3xl border overflow-hidden">
+                                <h3 className="text-lg font-bold mb-2 truncate">{item.title}</h3>
+                                <p className="text-sm text-gray-600 mb-2">{new Date(item.date * 1000).toLocaleDateString()}</p>
+                                {item.contents.includes('{STEAM_CLAN_IMAGE}') && (
+                                    <img
+                                        src={`https://steamcdn-a.akamaihd.net/steamcommunity/public/images/clans/${item.contents.match(/{STEAM_CLAN_IMAGE}\/([^ ]+)/)[1]}`}
+                                        alt="News"
+                                        className="mb-2 rounded"
+                                    />
+                                )}
+                                <div
+                                    className="mb-2 text-gray-700"
+                                    dangerouslySetInnerHTML={{
+                                        __html: item.contents.replace(
+                                            /{STEAM_CLAN_IMAGE}[^}]+} /g,
+                                            ''
+                                        ),
+                                    }}
+                                />
+                                <a href={item.url} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
+                                    Read more
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                    <Pagination
+                        cardsPerPage={newsPerPage}
+                        totalCards={news.length}
+                        paginate={handleNewsPageChange}
+                        currentPage={currentPage}
+                    />
+                </div>
+
+                <div className="daily-deals-section w-fit">
+                    <h2 className="text-xl font-bold mb-4">Daily Deals</h2>
+                    <div className="grid grid-cols-1 gap-4">
+                        {dailyDeals.map((deal, index) => (
+                            <div key={index} className="bg-white p-4 shadow-md rounded-3xl border overflow-hidden">
+                                <h3 className="text-lg font-bold mb-2 truncate">{deal.title}</h3>
+                                <img src={deal.header_image} alt={deal.title} className="mb-2 rounded" />
+                                <p className="text-sm text-gray-600 mb-2">Discount: {deal.discount_percent}%</p>
+                                <p className="text-sm text-gray-600 mb-2">Original Price: ${deal.original_price / 100}</p>
+                                <p className="text-sm text-gray-600 mb-2">Price: ${deal.final_price / 100}</p>
+                                <a href={deal.url} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
+                                    View Deal
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                    <Pagination
+                        cardsPerPage={dealsPerPage}
+                        totalCards={totalDeals}
+                        paginate={handleDealsPageChange}
+                        currentPage={dealsPage}
+                    />
+                </div>
+
+
+
+            </div>
+            <div className="new-releases-section mt-8">
+                <h2 className="text-xl font-bold mb-4">New Releases</h2>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {newReleases.map((release, index) => (
+                        <div key={index} className="bg-white p-4 shadow-md rounded-3xl border overflow-hidden">
+                            <h3 className="text-lg font-bold mb-2 truncate">{release.title}</h3>
+                            <img src={release.header_image} alt={release.title} className="mb-2 rounded" />
+                            <p className="text-sm text-gray-600 mb-2">Price: ${release.price}</p>
+                            <a href={release.url} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
+                                View Game
+                            </a>
+                        </div>
+                    ))}
+                </div>
+                <Pagination
+                    cardsPerPage={relPerPage}
+                    totalCards={newReleases}
+                    paginate={handleRelPageChange}
+                    currentPage={releaasePage}
+                />
+            </div>
+        </div>
     );
-}
+};
+
+export default HomePage;
--- src/Pages/ShowGames.jsx
+++ src/Pages/ShowGames.jsx
@@ -10,7 +10,6 @@ const ShowGames = () => {
     const [filteredGames, setFilteredGames] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [error, setError] = useState(null);
-    const [selectedType, setSelectedType] = useState('all');
     const gamesPerPage = 6;
     const excludedAppIds = [216938, 660010, 660130, 1122575, 1122577, 1122576, 1122579, 1122578, 254650, 254671, 254674, 254670, 254673, 254672];
 
@@ -97,21 +96,21 @@ const ShowGames = () => {
     };
 
     const indexOfLastGame = currentPage * gamesPerPage;
-    const indexOfFirstGame = indexOfLastGame - gamesPerPage;npm 
-    // const currentGames = filteredGames.slice(indexOfFirstGame, indexOfLastGame);
+    const indexOfFirstGame = indexOfLastGame - gamesPerPage;
+    const currentGames = filteredGames.slice(indexOfFirstGame, indexOfLastGame);
 
-    const filterGamesByType = () => {
-        if (selectedType === 'all') {
-            return filteredGames;
-        } else {
-            return filteredGames.filter(game =>
-                detailedGames[game.appid] && detailedGames[game.appid].type === selectedType
-            );
-        }
-    };
+    // const filterGamesByType = () => {
+    //     if (selectedType === 'all') {
+    //         return filteredGames;
+    //     } else {
+    //         return filteredGames.filter(game =>
+    //             detailedGames[game.appid] && detailedGames[game.appid].type === selectedType
+    //         );
+    //     }
+    // };
 
-    const filteredByTypeGames = filterGamesByType();
-    const currentGames = filteredByTypeGames.slice(indexOfFirstGame, indexOfLastGame);
+    // const filteredByTypeGames = filterGamesByType();
+    // const currentGames = filteredByTypeGames.slice(indexOfFirstGame, indexOfLastGame);
 
 
 
@@ -128,61 +127,32 @@ const ShowGames = () => {
                 />
             </div>
 
-            <div className='mb-4'>
-                <button
-                    className={`bg-black text-white m-2 p-2 ${selectedType === 'game' ? 'bg-gray-700' : ''}`}
-                    onClick={() => setSelectedType('game')}
-                >
-                    Games
-                </button>
-                <button
-                    className={`bg-black text-white m-2 p-2 ${selectedType === 'dlc' ? 'bg-gray-700' : ''}`}
-                    onClick={() => setSelectedType('dlc')}
-                >
-                    DLC
-                </button>
-                <button
-                    className={`bg-black text-white m-2 p-2 ${selectedType === 'demo' ? 'bg-gray-700' : ''}`}
-                    onClick={() => setSelectedType('demo')}
-                >
-                    Demo
-                </button>
-                <button
-                    className={`bg-black text-white m-2 p-2 ${selectedType === 'mod' ? 'bg-gray-700' : ''}`}
-                    onClick={() => setSelectedType('mod')}
-                >
-                    Mods
-                </button>
-                <button
-                    className={`bg-black text-white m-2 p-2 ${selectedType === 'all' ? 'bg-gray-700' : ''}`}
-                    onClick={() => setSelectedType('all')}
-                >
-                    All
-                </button>
-            </div>
-
+            {/* I should add a button to handle the type of each game */}
 
             {error && <p className="text-red-500">{error}</p>}
 
             <div className="grid grid-cols-3 gap-4">
-                {currentGames
-                    .filter(game => detailedGames[game.appid] && detailedGames[game.appid].header_image)
-                    .map(game => (
-                        <Link to={`/games/${game.appid}`} key={game.appid} className="bg-white p-4 rounded shadow-md">
-                            <div className="flex flex-col items-center">
-                                <img
-                                    src={detailedGames[game.appid].header_image}
-                                    className="mb-2"
-                                    alt={game.name}
-                                />
-                                <p className="text-center font-bold">{game.name}</p>
-                                <p className="text-center text-sm text-gray-600">
-                                    {detailedGames[game.appid].type || 'Unknown Type'}
-                                </p>
-                            </div>
-                        </Link>
-                    ))}
+                {currentGames.map(game => (
+                    <Link to={`/games/${game.appid}`} key={game.appid} className="bg-white p-4 rounded shadow-md">
+                        <div className="flex flex-col items-center">
+                            {detailedGames[game.appid] && (
+                                <>
+                                    <img
+                                        src={detailedGames[game.appid].header_image}
+                                        className="mb-2"
+                                    />
+                                    <p className="text-center font-bold">{game.name}</p>
+                                    <p className="text-center text-sm text-gray-600">
+                                        {detailedGames[game.appid].type || 'Unknown Type'}
+                                    </p>
+                                </>
+                            )}
+                        </div>
+                    </Link>
+                ))}
             </div>
+
+
 
             <Pagination
                 cardsPerPage={gamesPerPage}
