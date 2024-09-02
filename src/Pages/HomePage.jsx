652bd2f1b8ee91dafb02407c291cb25152295de7
Amine lehkim. <aminelehkim158@gmail.com>
2024-09-02 19:19:52 +0100

    changing the hero section

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -11,11 +11,19 @@ const HomePage = () => {
     const [releaasePage, setReleasePage] = useState(1);
     const [totalDeals, setTotalDeals] = useState(0);
     const [totalReleases, setTotalReleases] = useState(0);
+    const [searchResults, setSearchResults] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
     const [error, setError] = useState(null);
+    
     const newsPerPage = 4;
     const dealsPerPage = 2;
     const relPerPage = 2;
 
+    const clearSearch = () => {
+        setSearchQuery('');
+        setSearchResults([]);
+    };
+    
     useEffect(() => {
         const fetchNews = async () => {
             try {
@@ -70,14 +78,98 @@ const HomePage = () => {
         setReleasePage(pageNumber);
     };
 
+    const handleSearch = async (query) => {
+        try {
+            const response = await axios.get(`http://localhost:4000/api/search-games?query=${query}`);
+            setSearchResults(response.data);
+        } catch (err) {
+            console.error('Error searching for games:', err.message);
+            setError('Error searching for games');
+        }
+    };
+
     return (
         <div className="container mx-auto p-4">
+
+            <div className="relative content-center justify-center h-screen w-full m-4">
+                <img
+                    src="https://images.unsplash.com/photo-1494783367193-149034c05e8f"
+                    alt="Background Image"
+                    className="absolute inset-0 w-full h-full object-cover filter blur-sm bg"
+                />
+                <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+                <div className="absolute bg-inherit inset-0 top-16">
+                    <div className="dark:bg-transparent">
+                        <div className="mx-auto flex flex-col items-center py-12 sm:py-24">
+                            <div className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col mb-5 sm:mb-10">
+                                <h1 className="text-4xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-6xl text-center text-gray-800 dark:text-white font-black leading-10">
+                                    Lets not stress for{' '}
+                                    <span className="text-violet-800 dark:text-violet-500">Website</span>{' '}
+                                    designs.
+                                </h1>
+                                <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-600 dark:text-gray-300 font-normal text-center text-xl">
+                                    A Community-built Tailwind component library.
+                                </p>
+                            </div>
+                            <div className="flex w-11/12 md:w-8/12 xl:w-6/12">
+                                <div className="flex flex-col rounded-md w-full">
+                                    <div className="flex rounded-md w-full">
+                                        <input
+                                            type="text"
+                                            name="q"
+                                            value={searchQuery}
+                                            onChange={(e) => setSearchQuery(e.target.value)}
+                                            className="w-full p-3 rounded-md rounded-r-none border border-white placeholder-current bg-inherit dark:text-gray-300"
+                                            placeholder="keyword"
+                                        />
+                                        <button
+                                            onClick={handleSearch}
+                                            className="inline-flex items-center gap-2 bg-violet-700 text-white border-white text-lg font-semibold py-3 px-6 rounded-r-md"
+                                        >
+                                            <span>Find</span>
+                                            <svg
+                                                className="text-gray-200 h-5 w-5 p-0 fill-current"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 56.966 56.966"
+                                            >
+                                                <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+                                            </svg>
+                                        </button>
+                                        <button
+                                            onClick={clearSearch}
+                                            className="ml-2 inline-flex items-center gap-2 bg-gray-500 text-white border-white text-lg font-semibold py-3 px-6 rounded-md"
+                                        >
+                                            Clear
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            {searchResults.length > 0 && (
+                                <div className="w-11/12 md:w-8/12 xl:w-6/12 mx-auto bg-white p-4 shadow-md rounded-3xl border overflow-auto h-60 mt-4">
+                                    <h3 className="text-lg font-bold mb-2 text-center">Search Results</h3>
+                                    <div className="flex flex-col gap-4">
+                                        {searchResults.map((result, index) => (
+                                            <div key={index} className="bg-gray-100 p-4 shadow rounded-md">
+                                                <h4 className="text-md font-bold">{result.name}</h4>
+                                                <p>{result.appid}</p>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
             <h1 className="text-2xl font-bold mb-4">Latest News & Daily Deals</h1>
 
             {error && <p className="text-red-500">{error}</p>}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
 
-                <div className="new-releases-section w-fit">
+                <div className="new-releases-section w-fit border border-spacing-8 border-black border-b-secondary-400 rounded-3xl p-4">
                     <h2 className="text-xl font-bold mb-4">New Releases</h2>
                     <div className="grid grid-cols-1 gap-4">
                         {newReleases.map((release, index) => (
@@ -94,13 +186,13 @@ const HomePage = () => {
 
                     <Pagination
                         cardsPerPage={relPerPage}
-                        totalCards={totalReleases}  
+                        totalCards={totalReleases}
                         paginate={handleRelPageChange}
-                        currentPage={releaasePage}  
+                        currentPage={releaasePage}
                     />
                 </div>
 
-                <div className="daily-deals-section w-fit">
+                <div className="daily-deals-section w-fit border border-spacing-8 border-black border-b-secondary-400 rounded-3xl p-4">
                     <h2 className="text-xl font-bold mb-4">Daily Deals</h2>
                     <div className="grid grid-cols-1 gap-4">
                         {dailyDeals.map((deal, index) => (
@@ -126,7 +218,7 @@ const HomePage = () => {
             </div>
 
             {/* News Section */}
-            <div className="news-section w-fit">
+            <div className="news-section w-fit m-5">
                 <h2 className="text-xl font-bold mb-4">Latest News</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                     {currentNews.map((item, index) => (
@@ -165,6 +257,7 @@ const HomePage = () => {
 
 
         </div>
+
     );
 };
 
