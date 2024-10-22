9ea9958cb6dfeb49e731a52b45e22638042f9ed2
Amine lehkim <74158555+Darknessly1@users.noreply.github.com>
2024-10-22 15:36:47 +0100

    fixing bugs and redesign the classes page

--- src/Pages/wowpages/ClassPage.jsx
+++ src/Pages/wowpages/ClassPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
@@ -13,6 +13,10 @@ export default function ClassPage() {
     const [error, setError] = useState(null);
     const [specializationMedia, setSpecializationMedia] = useState({});
     const [expandedSpecId, setExpandedSpecId] = useState(null); // State for expanded specialization
+    const [showPopup, setShowPopup] = useState(false);
+    const popupRef = useRef(null);
+
+
 
     useEffect(() => {
         const fetchToken = async () => {
@@ -108,12 +112,33 @@ export default function ClassPage() {
         fetchData();
     }, [classId]);
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>{error}</div>;
 
-    const handleToggle = (id) => {
-        setExpandedSpecId(prevId => (prevId === id ? null : id));
-    };
+
+    // Handle clicking outside the pop-up
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (popupRef.current && !popupRef.current.contains(event.target)) {
+                setShowPopup(false);  // Close the popup when clicking outside
+            }
+        };
+
+        // Attach event listener only if the popup is shown
+        if (showPopup) {
+            document.addEventListener('mousedown', handleClickOutside);
+        } else {
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+
+        // Clean up the event listener when component unmounts
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [showPopup]);
+
+
+    if (loading) return <div>Loading...</div>;
+
+    if (error) return <div>{error}</div>;
 
     return (
         <>
@@ -124,8 +149,11 @@ export default function ClassPage() {
             <div className="class-details p-6">
                 <div className="mt-4">
                     <Link to={`/classespage`}>
-                        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-                            Back to Classes
+                        <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                                Back to Classes
+                            </span>
                         </button>
                     </Link>
                 </div>
@@ -145,25 +173,54 @@ export default function ClassPage() {
                     </h3>
                 </div>
 
-                <div className='my-4'>
-                    <h3 className='font-bold text-xl'>Playable Races:</h3>
-                    <ul className='list-disc ml-6'>
-                        {classDetails.playable_races?.map(race => (
-                            <button
-                                key={race.id}
-                                className='m-3 p-1 border-2 border-black rounded-lg'
+                <div className="my-4">
+                    <h3 className="font-bold text-xl">Playable Races:</h3>
+                    <button
+                        className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-lg"
+                        onClick={() => setShowPopup(true)}
+                    >
+                        Show Playable Races
+                    </button>
+
+                    {showPopup && (
+                        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                            <div
+                                className="bg-white p-6 rounded-lg  relative shadow-lg"
+                                ref={popupRef}
                             >
-                                {race.name}
-                            </button>
-                        ))}
-                    </ul>
+                                <button
+                                    className="absolute top-2 right-2 text-red-500 font-bold"
+                                    onClick={() => setShowPopup(false)}
+                                >
+                                    X
+                                </button>
+
+                                <h3 className="font-bold text-xl mb-4">Playable Races</h3>
+                                <div className="grid grid-cols-5 gap-4">
+                                    {classDetails.playable_races?.map((race) => (
+                                        <button
+                                            key={race.id}
+                                            className="border-2 border-black p-2 rounded-lg text-center"
+                                        >
+                                            {race.name}
+                                        </button>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
 
+
                 <h2 className="text-2xl font-bold mb-6">Specializations</h2>
-                <div className="specialization-details grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="specialization-details grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 relative">
                     {specializationDetails.map(spec => (
-                        <div key={spec.id} className="card bg-white rounded-lg shadow-md p-4">
-                            <div className="flex items-center justify-between cursor-pointer">
+                        <div
+                            key={spec.id}
+                            className={`card rounded-lg shadow-md p-4 transition-all duration-300 ${expandedSpecId === spec.id ? 'bg-blue-600' : 'bg-white'
+                                }`}
+                        >
+                            <div className="flex items-center justify-between">
                                 <div className="flex items-center">
                                     {specializationMedia[spec.id] && (
                                         <img
@@ -176,38 +233,48 @@ export default function ClassPage() {
                                 </div>
                                 <button
                                     onClick={() => setExpandedSpecId(expandedSpecId === spec.id ? null : spec.id)}
-                                    className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+                                    className={`px-4 py-2 text-white rounded-lg hover:bg-blue-600 transition ${expandedSpecId === spec.id ? 'bg-red-500' : 'bg-blue-500'
+                                        }`}
                                 >
                                     {expandedSpecId === spec.id ? 'Hide Details' : 'Show Details'}
                                 </button>
                             </div>
-                            {expandedSpecId === spec.id && (
-                                <div className="expanded-content mt-4 w-full">
-                                    <p className="mb-2">
-                                        <strong>Role:</strong> {spec.role?.name}
-                                    </p>
-                                    <div className="mb-4">
-                                        <h4 className="font-bold text-3xl text-red-500">PvP Talents:</h4>
-                                        <ul className="list-disc list-inside">
-                                            {spec.pvp_talents?.map((talent, index) => (
-                                                <div key={index} className="mb-2">
-                                                    <h5 className="font-semibold">{talent.talent.name}</h5>
-                                                    <p className="text-sm">
-                                                        <strong>Description:</strong> {talent.spell_tooltip.description}
-                                                    </p>
-                                                </div>
-                                            ))}
-                                        </ul>
-                                    </div>
-                                    <p className="mb-2">
-                                        <strong>Primary Stat:</strong> {spec.primary_stat_type?.name}
-                                    </p>
-                                </div>
-                            )}
                         </div>
                     ))}
-                </div>
 
+                    {expandedSpecId !== null && (
+                        <div className="expanded-content mt-6 col-span-3 bg-blue-200 rounded-lg shadow-md p-6">
+                            {specializationDetails.map(spec => {
+                                if (spec.id === expandedSpecId) {
+                                    return (
+                                        <div key={spec.id}>
+                                            <p className="mb-2">
+                                                <strong>Role:</strong> {spec.role?.name}
+                                            </p>
+                                            <div className="mb-4">
+                                                <h4 className="font-bold text-3xl text-red-500">PvP Talents:</h4>
+                                                <ul className="list-disc list-inside">
+                                                    {spec.pvp_talents?.map((talent, index) => (
+                                                        <div key={index} className="mb-2">
+                                                            <h5 className="font-semibold">{talent.talent.name}</h5>
+                                                            <p className="text-sm">
+                                                                <strong>Description:</strong> {talent.spell_tooltip.description}
+                                                            </p>
+                                                        </div>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                            <p className="mb-2">
+                                                <strong>Primary Stat:</strong> {spec.primary_stat_type?.name}
+                                            </p>
+                                        </div>
+                                    );
+                                }
+                                return null;
+                            })}
+                        </div>
+                    )}
+                </div>
             </div>
         </>
     );
--- src/Pages/wowpages/PlayableRacesPage.jsx
+++ src/Pages/wowpages/PlayableRacesPage.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import WowNav from "../../Headers/WowNav";
 
 export default function PlayableRaces() {
-
     const [playableRaces, setPlayableRaces] = useState([]);
+    const [showClasses, setShowClasses] = useState(null); // Manage which race's classes to show
+    const ref = useRef(); // Create a ref for the classes div
 
     useEffect(() => {
         const fetchToken = async () => {
@@ -17,7 +18,7 @@ export default function PlayableRaces() {
                             'Content-Type': 'application/x-www-form-urlencoded',
                         },
                         auth: {
-                            username: '0b9ab91266f7473693771094a1dbbad4',
+                            username: '0b9ab91266f7473693771094a1dbbad4',  // Use environment variables for security
                             password: 'eouiAwYmulo3RW7yP3E5ZTC9rKR1g7Ex',
                         },
                     }
@@ -28,6 +29,7 @@ export default function PlayableRaces() {
                 throw error;
             }
         };
+
         const fetchPlayableRaces = async (token) => {
             try {
                 const response = await axios.get(
@@ -45,49 +47,20 @@ export default function PlayableRaces() {
 
                 const raceDetails = await Promise.all(
                     response.data.races.map(async (race) => {
-                        try {
-                            const detailsResponse = await axios.get(
-                                `https://us.api.blizzard.com/data/wow/playable-race/${race.id}`,
-                                {
-                                    headers: {
-                                        Authorization: `Bearer ${token}`,
-                                    },
-                                    params: {
-                                        namespace: 'static-us',
-                                        locale: 'en_US',
-                                    },
-                                }
-                            );
+                        const detailsResponse = await axios.get(
+                            `https://us.api.blizzard.com/data/wow/playable-race/${race.id}`,
+                            {
+                                headers: {
+                                    Authorization: `Bearer ${token}`,
+                                },
+                                params: {
+                                    namespace: 'static-us',
+                                    locale: 'en_US',
+                                },
+                            }
+                        );
 
-                            const mediaResponse = await axios.get(
-                                `https://us.api.blizzard.com/data/wow/media/character-race/${race.id}`,
-                                {
-                                    headers: {
-                                        Authorization: `Bearer ${token}`,
-                                    },
-                                    params: {
-                                        namespace: 'static-us',
-                                        locale: 'en_US',
-                                    },
-                                }
-                            );
-
-                            const imageUrl = mediaResponse.data.assets[0].value;
-
-                            return {
-                                ...detailsResponse.data,
-                                imageUrl,
-                                faction: detailsResponse.data.faction.name,
-                                startingZone: detailsResponse.data.starting_zone.name,
-                                availableClasses: detailsResponse.data.playable_classes.map((cls) => cls.name),
-                            };
-                        } catch (error) {
-                            console.error(`Error fetching details for race ${race.id}:`, error);
-                            return {
-                                ...race,
-                                imageUrl: null,
-                            };
-                        }
+                        return detailsResponse.data; // Return detailed race data
                     })
                 );
 
@@ -97,18 +70,35 @@ export default function PlayableRaces() {
             }
         };
 
+
         const fetchData = async () => {
             try {
                 const token = await fetchToken();
                 await fetchPlayableRaces(token);
             } catch (error) {
-                console.error(error)
+                console.error(error);
             }
-        }
+        };
 
         fetchData();
+    }, []);
 
-    }, [])
+
+
+    // Existing useEffect and API fetch code...
+
+    // Event handler to close the classes div when clicking outside
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (ref.current && !ref.current.contains(event.target)) {
+                setShowClasses(null);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [ref]);
 
     return (
         <>
@@ -117,66 +107,68 @@ export default function PlayableRaces() {
             </div>
 
             <div className="playable-races-section mb-8">
-                <div
-                    className="bg-gray-800 text-white p-4 rounded-xl w-fit m-4 "
-                >
+                <div className="bg-gray-800 text-white p-4 rounded-xl w-fit m-4">
                     <h2 className="text-2xl font-bold">Playable Races</h2>
                 </div>
-                <>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 mt-4">
-                        {playableRaces && playableRaces.length > 0 ? (
-                            playableRaces.map((race) => (
-                                <div
-                                    key={race.id}
-                                    className="bg-white p-4 shadow-md rounded-2xl border overflow-hidden"
-                                >
-                                    {/* Display race name */}
-                                    <h2 className="text-xl font-bold mb-2 text-center">{race.name}</h2>
-
-                                    {/* Display race image */}
-                                    {race.imageUrl && (
-                                        <img
-                                            src={race.imageUrl}
-                                            alt={race.name}
-                                            className="mb-4 w-full h-48 object-cover rounded-xl"
-                                        />
-                                    )}
-
-                                    {/* Display race faction */}
-                                    <p className="text-sm text-gray-600 mb-2">
-                                        Faction: {race.faction}
-                                    </p>
-
-                                    {/* Display starting zone */}
-                                    <p className="text-sm text-gray-600 mb-2">
-                                        Starting Zone: {race.startingZone}
-                                    </p>
-
-                                    {/* Display available classes */}
-                                    {race.availableClasses && race.availableClasses.length > 0 ? (
-                                        <p className="text-sm text-gray-600 mb-2">
-                                            Available Classes: {race.availableClasses.join(', ')}
-                                        </p>
-                                    ) : (
-                                        <p className="text-sm text-gray-600 mb-2">
-                                            No available classes
-                                        </p>
-                                    )}
-
-                                    {/* Display race description */}
-                                    <p className="text-sm text-gray-600 mb-2">
-                                        Description: {race.description}
-                                    </p>
-                                </div>
-                            ))
-                        ) : (
-                            <p className="text-center text-green-500">
-                                Loading...
-                            </p>
-                        )}
-                    </div>
-                </>
-            </div>
+                <div className="overflow-x-auto">
+                    <table className="min-w-full divide-y divide-gray-200 rounded-lg shadow-lg">
+                        <thead className="bg-gray-300">
+                            <tr>
+                                <th className="px-4 py-2 text-left text-sm font-medium text-gray-700">Race Name</th>
+                                <th className="px-4 py-2 text-left text-sm font-medium text-gray-700">M and F Name</th>
+                                <th className="px-4 py-2 text-left text-sm font-medium text-gray-700">Faction</th>
+                                <th className="px-4 py-2 text-left text-sm font-medium text-gray-700">Selectable</th>
+                                <th className="px-4 py-2 text-left text-sm font-medium text-gray-700">Available Classes</th>
+                            </tr>
+                        </thead>
+                        <tbody className="bg-white divide-y divide-gray-200">
+                            {playableRaces && playableRaces.length > 0 ? (
+                                playableRaces.map((race, index) => (
+                                    <tr key={race.id} className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}>
+                                        <td className="px-4 py-2 whitespace-nowrap text-sm text-gray-700">{race.name}</td>
+                                        <td className="px-4 py-2 whitespace-nowrap text-sm text-gray-700">{race.gender_name.male}</td>
+                                        <td className="px-4 py-2 whitespace-nowrap text-sm text-gray-700">{race.faction.name}</td>
+                                        <td className="px-4 py-2 whitespace-nowrap text-sm text-gray-700">{race.is_selectable ? 'Yes' : 'No'}</td>
+                                        <td className="px-4 py-2 whitespace-nowrap">
+                                            <button
+                                                onClick={() => setShowClasses(race.id === showClasses ? null : race.id)} // Toggle the classes div
+                                                className="bg-blue-500 text-white px-2 py-1 rounded-md"
+                                            >
+                                                {showClasses === race.id ? 'Hide Classes' : 'Show Classes'}
+                                            </button>
+                                            {showClasses === race.id && (
+                                                <div className="fixed inset-0 bg-blue-gray-200 bg-opacity-40 flex justify-center items-center">
+                                                    <div
+                                                        ref={ref}
+                                                        className="bg-gray-700 p-4 rounded-md shadow-md flex h-fit"
+                                                    >
+                                                        {race.playable_classes.map((cls) => (
+                                                            <button
+                                                                key={cls.id}
+                                                                className="bg-blue-500 text-white px-2 py-1 rounded-md m-1 hover:bg-blue-600"
+                                                            >
+                                                                {cls.name}
+                                                            </button>
+                                                        ))}
+                                                    </div>
+                                                </div>
+                                            )}
+                                        </td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan="6" className="text-center text-green-500 py-4">
+                                        Loading...
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </div >
         </>
-    )
+    );
+
+
 }
